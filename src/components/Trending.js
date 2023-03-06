b5cbb7de9411b6a22009e4f16b8e3a20fce46931
Angelo Ian Michael Cardona <62699400+ianiscardona@users.noreply.github.com>
2023-03-06 14:34:09 +0800

    tentative final update

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,24 +1,25 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { TrendWeek } from "./TrendingWeek";
 import { TrendThrowback } from "./TrendingTB";
 import AnimeData from "../data/AnimeData";
 import ThrowbackData from "../data/ThrowbackData";
-import { Link } from "react-router-dom";
-import slugify from "react-slugify";
 
 export const Trending = (props) => {
+  const [trendingItemValue, setTrendingItemValue] = useState("");
   // properties
-  const trending = AnimeData.map((details) => {
+  const trending = AnimeData.filter((details) => {
+    return trendingItemValue === ""
+      ? details
+      : details.name.toLowerCase().includes(trendingItemValue.toLowerCase());
+  }).map((details) => {
     return (
-      <Link to={`${slugify(details.name)}`}>
-        <TrendWeek
-          key={details.id}
-          name={details.name}
-          rating={details.rating}
-          categories={details.categories}
-          image={details.image}
-        />
-      </Link>
+      <TrendWeek
+        key={details.id}
+        name={details.name}
+        rating={details.rating}
+        categories={details.categories}
+        image={details.image}
+      />
     );
   });
 
@@ -37,7 +38,7 @@ export const Trending = (props) => {
 
   return (
     <div className="z-0 py-16 relative bg-stone-900">
-      <div className="mx-20 w-auto pb-7">
+      <div className="mx-20 w-auto pb-7 flex justify-between">
         <button
           className="text-white text-[34px] leading-none font-bold font-outfit tracking-wide antialiased"
           onClick={() => scrollToSection(trendingRef)}
@@ -45,6 +46,14 @@ export const Trending = (props) => {
           Trending
           <text className="text-[#FBC94A]"> this week</text>
         </button>
+        <div>
+          <input
+            type="search"
+            placeholder="Search..."
+            className="rounded-lg px-3 py-1 text-[#106580]"
+            onChange={(e) => setTrendingItemValue(e.target.value)}
+          />
+        </div>
       </div>
       <section
         className="px-20 pb-2 flex space-x-5 mb-10 space-y-3 flex-nowrap overflow-x-auto scrollbar-hide snap-x scroll-pl-20"
--- src/components/TrendingWeek.js
+++ src/components/TrendingWeek.js
@@ -1,53 +1,45 @@
 import { FaStar } from "react-icons/fa";
 import React from "react";
-import { useNavigate } from "react-router-dom";
-// import { TrendWeekSeriesModal } from "./TrendWSeriesModal";
+import { NavLink, useNavigate } from "react-router-dom";
+import slugify from "react-slugify";
 
 export const TrendWeek = (props) => {
   const navigate = useNavigate();
-  // const [openModal, setOpenModal] = useState(false);
-
-  // function open() {
-  //   setOpenModal(true);
-  // }
-  // function close() {
-  //   setOpenModal(false);
-  // }
-
   return (
-    <div className="">
-      {/* {openModal && <TrendWeekSeriesModal close={close} />} */}
-      <button
-        onClick={() => navigate(":name")}
-        className="w-[600px] h-[337px] rounded-3xl relative overflow-hidden hover:shadow-[0_4px_10px_-3px_rgba(196,196,196,0.4)] hover:ease-out hover:duration-300 snap-start"
-      >
-        <img
-          src={`../images/${props.image}`}
-          className="h-full w-full object-cover absolute"
-          alt=""
-        ></img>
-        <div className="bg-gradient-to-t from-black w-full h-full absolute"></div>
-        <div className="px-7 pb-5 h-full flex relative justify-end items-start flex-col">
-          <h1 className="text-white text-2xl leading-none font-bold font-outfit tracking-wide antialiased">
-            {props.name}
-          </h1>
-          <ul className="flex items-end w-full justify-between">
-            <li>
-              <p className="text-white text-xs font-normal font-outfit antialiased">
-                {props.categories}
-              </p>
-            </li>
-            <li>
-              <p className="flex items-end justify-center content-between space-x-1 text-white text-xs font-normal font-outfit antialiased">
-                <div className="text-[#FBC94A]">
-                  <FaStar size={20} />
-                </div>
-                <div>{props.rating}</div>
-              </p>
-            </li>
-          </ul>
-        </div>
-      </button>
-    </div>
+    <>
+      <NavLink to={`${slugify(props.name)}`}>
+        <button
+          onClick={() => navigate(":name")}
+          className="w-[600px] h-[337px] rounded-3xl relative overflow-hidden hover:shadow-[0_4px_10px_-3px_rgba(196,196,196,0.4)] hover:ease-out hover:duration-300 snap-start"
+        >
+          <img
+            src={`../images/${props.image}`}
+            className="h-full w-full object-cover absolute"
+            alt=""
+          ></img>
+          <div className="bg-gradient-to-t from-black w-full h-full absolute"></div>
+          <div className="px-7 pb-5 h-full flex relative justify-end items-start flex-col">
+            <h1 className="text-white text-2xl leading-none font-bold font-outfit tracking-wide antialiased">
+              {props.name}
+            </h1>
+            <ul className="flex items-end w-full justify-between">
+              <li>
+                <p className="text-white text-xs font-normal font-outfit antialiased">
+                  {props.categories}
+                </p>
+              </li>
+              <li>
+                <p className="flex items-end justify-center content-between space-x-1 text-white text-xs font-normal font-outfit antialiased">
+                  <div className="text-[#FBC94A]">
+                    <FaStar size={20} />
+                  </div>
+                  <div>{props.rating}</div>
+                </p>
+              </li>
+            </ul>
+          </div>
+        </button>
+      </NavLink>
+    </>
   );
 };
